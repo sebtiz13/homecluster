6259124d9b6e77480b4a8bcfc3d9f1b46a66f948
orblazer <orblazer.minecraft@gmail.com>
2022-10-24 15:49:28 +0200

    ci: support devel charts channel

--- scripts/charts/deploy.sh
+++ scripts/charts/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # helm dependency build --skip-refresh "$1" #? its run on lint
-helm cm-push "$1" my-repo
+helm cm-push --username "$CI_REGISTRY_USER" --password "$CI_REGISTRY_PASSWORD" \
+  "$1" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/helm/${HELM_CHANNEL}"
