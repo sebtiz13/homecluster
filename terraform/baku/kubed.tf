500bfb9765020cbeeda9dff0f8e1ad64fe7f2b91
orblazer <orblazer.minecraft@gmail.com>
2022-11-01 12:08:34 +0100

    fix(baku): use right provider for kubed

--- terraform/baku/kubed.tf
+++ terraform/baku/kubed.tf
@@ -1,5 +1,6 @@
 resource "kubectl_manifest" "kubed" {
   depends_on = [argocd_cluster.kubernetes]
+  provider   = kubectl.salamandre
 
   override_namespace = local.argocd_namespace
   yaml_body          = file("${var.manifests_folder}/kubed.yaml")
