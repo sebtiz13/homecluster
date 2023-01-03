be1be779d016ffc378193db8da5d0f6485c91205
orblazer <orblazer.minecraft@gmail.com>
2023-01-03 13:39:02 +0100

    feat(ansible): create script for db user

---
+++ ansible/roles/database/files/create-user.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+  echo "Usage: create-db-user <username>"
+  exit 1
+fi
+
+createDb() {
+  sudo -u postgres psql -c "\set AUTOCOMMIT on\n
+CREATE DATABASE $1;
+CREATE USER $1 WITH ENCRYPTED PASSWORD '$2';
+GRANT ALL PRIVILEGES ON DATABASE $1 TO $1;"
+}
+
+password=$(head -c 512 /dev/urandom | LC_CTYPE=C tr -cd "a-zA-Z0-9" | head -c 16)
+if createDb "$1" "$password"; then
+  echo "New User: $1"
+  echo "With password: $password"
+fi
