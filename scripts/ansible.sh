8c6e8901930815d86e33fa3044e05fd520108ccb
orblazer <orblazer.minecraft@gmail.com>
2023-07-21 16:51:15 +0200

    fix(ansible): right validate certificate for vault calls

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -10,6 +10,7 @@ cd ansible || return
 . ./.venv/bin/activate
 if [ "$ENVIRONMENT" == "dev" ]; then
   export ANSIBLE_HOST_KEY_CHECKING=false
+  export ANSIBLE_HASHI_VAULT_CA_CERT=$(realpath ../vagrant/.vagrant/ca/rootCA.pem)
 fi
 ansible-playbook --inventory "inventories/$ENVIRONMENT" \
   --extra-vars "root_domain='${DOMAIN_NAME}'" $1 site.yaml
