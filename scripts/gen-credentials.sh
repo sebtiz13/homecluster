f70b80b2afd82f28406eba04cc189382ac90da04
orblazer <orblazer.minecraft@gmail.com>
2024-08-24 17:59:45 +0200

    fix(scripts): add missing discord webhook url question

--- scripts/gen-credentials.sh
+++ scripts/gen-credentials.sh
@@ -27,13 +27,13 @@ insert_pwd() {
 
 mkdir -p "./out/credentials/${ENVIRONMENT}"
 
+##
 # Admin passwords
+##
 FILE="./out/credentials/${ENVIRONMENT}/admin_passwords.yaml"
 touch "$FILE"
 
-##
 # Salamandre credentials
-##
 insert_pwd "$FILE" .salamandre.zitadel "$(randpw 16)"
 insert_pwd "$FILE" .salamandre.nextcloud "$(randpw 16)"
 insert_pwd "$FILE" .salamandre.forgejo "$(randpw 16)"
@@ -45,20 +45,32 @@ if [[ "$(yq .salamandre.vaultwarden "$FILE")" = "null" ]]; then
   unset "$token"
 fi
 
-##
 # Baku credentials
-##
 insert_pwd "$FILE" .baku.minio "$(randpw 16)"
 insert_pwd "$FILE" .baku.grafana "$(randpw 16)"
 
+##
 # Zitadel masterkey
+##
 FILE="./out/credentials/${ENVIRONMENT}/zitadel_masterkey"
 if [ ! -e "$FILE" ]; then
   pwgen -cn 32 1 > "$FILE"
 fi
 
-# Zitadel masterkey
+##
+# Forgejo runner token
+##
 FILE="./out/credentials/${ENVIRONMENT}/forgejo_runner_token"
 if [ ! -e "$FILE" ]; then
   openssl rand -hex 20 > "$FILE"
+fi
+
+##
+# Discord webhook url
+##
+FILE="./out/credentials/${ENVIRONMENT}/discord_webhook_url"
+if [ ! -e "$FILE" ]; then
+  echo "Enter discord webhook url:"
+  IFS= read -r webhookUrl
+  echo "$webhookUrl" > "$FILE"
 fi
