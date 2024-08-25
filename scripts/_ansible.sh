b33bb1866541db9a680dd49f1f8ecc7321236fe2
orblazer <orblazer.minecraft@gmail.com>
2024-08-25 17:08:11 +0200

    fix(ansible): right configure dev ssh
    This prevent error when using ssh-agent

--- scripts/_ansible.sh
+++ scripts/_ansible.sh
@@ -15,6 +15,6 @@ fi
 # shellcheck disable=SC1091
 . ./.venv/bin/activate
 # shellcheck disable=SC2086
-ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook --inventory "inventories/$2" \
+ansible-playbook --inventory "inventories/$2" \
   --extra-vars "root_domain=$3" --extra-vars "git_branch=$git_branch" \
 	$rest "$step.yaml"
